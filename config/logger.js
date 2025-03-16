22aa32dc230d5d699cb328a448b799c2f418cb4b
CHEROSA <rime.bahlak@gmail.com>
2025-03-16 04:05:17 -0500

    Delete config directory

--- config/logger.js
+++
@@ -1,9 +0,0 @@
-/**
- * Logger Configuration
- * 
- * This file re-exports the logger from utils/logger.js for backward compatibility.
- */
-
-const logger = require('../utils/logger');
-
-module.exports = logger; 
